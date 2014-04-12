38c55fa51fb57e702f8f8b84fa01f02aee6eb9a8
Bufas <bufas7@gmail.com>
2014-04-12 22:05:38 +0200

    Do FIND in situ, slight performance improvement

--- h1/find.py
+++ h1/find.py
@@ -1,21 +1,25 @@
 import random
 
-def find(L, k, d):
+def find(L, a, b, k, d):
     """Finds the number with rank k, and reports recursion depth"""
     # Select pivot
-    e = L[random.randint(0, len(L) - 1)] # TODO use true randomness
+    r = random.randint(a, b) # TODO use true randomness
+    L[r], L[b] = L[b], L[r] # Swap e and last element
+    e = L[b]
 
-    # Partition the list
-    L1 = []
-    L2 = []
-    for x in L:
-        if x < e: L1.append(x)
-        if x > e: L2.append(x)
+    ptr = a
+    for i in xrange(a,b):
+        if L[i] < e:
+            L[ptr], L[i] = L[i], L[ptr]
+            ptr += 1
+
+    # Swap e to its right place
+    L[ptr], L[b] = L[b], L[ptr]
 
     # Are we finished? If not, recurse
-    if len(L1) == k: return d, e
-    return find(L1, k, d+1) if len(L1) > k else find(L2, k - len(L1) - 1, d+1)
-
+    L1_size = ptr - a
+    if L1_size == k: return d, L[ptr]
+    return find(L, a, ptr-1, k, d+1) if L1_size > k else find(L, ptr+1, b, k - L1_size - 1, d+1)
 
 def test():
     # Build list of distinct numbers
@@ -24,7 +28,7 @@ def test():
     # Run experiments
     for _ in xrange(10):
         random.shuffle(L)
-        print sum([find(L, 500, 0)[0] for _ in xrange(1000)]) / 1000.0
+        print sum([find(L, 0, len(L)-1, 500, 0)[0] for _ in xrange(1000)]) / 1000.0
 
 
 test()
