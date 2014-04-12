d0768c8c68fd704eeccadd99e2ecc0d1591ea28e
Bufas <bufas7@gmail.com>
2014-04-12 23:04:38 +0200

    Iterative approach, also reports number of comparisons

--- h1/find.py
+++ h1/find.py
@@ -1,25 +1,41 @@
 import random
 
-def find(L, a, b, k, d):
+def find(L, k):
     """Finds the number with rank k, and reports recursion depth"""
-    # Select pivot
-    r = random.randint(a, b) # TODO use true randomness
-    L[r], L[b] = L[b], L[r] # Swap e and last element
-    e = L[b]
 
-    ptr = a
-    for i in xrange(a,b):
-        if L[i] < e:
-            L[ptr], L[i] = L[i], L[ptr]
-            ptr += 1
+    a = 0
+    b = len(L)-1
+    depth = 0
+    e = -1
+    comp = 0
 
-    # Swap e to its right place
-    L[ptr], L[b] = L[b], L[ptr]
+    while True:
+        # Select pivot
+        r = random.randint(a, b) # TODO use true randomness
 
-    # Are we finished? If not, recurse
-    L1_size = ptr - a
-    if L1_size == k: return d, L[ptr]
-    return find(L, a, ptr-1, k, d+1) if L1_size > k else find(L, ptr+1, b, k - L1_size - 1, d+1)
+        L[r], L[b] = L[b], L[r] # Swap e and last element
+        e = L[b]
+
+        ptr = a
+        for i in xrange(a,b):
+            comp += 1
+            if L[i] < e:
+                L[ptr], L[i] = L[i], L[ptr]
+                ptr += 1
+
+        # Swap e to its right place
+        L[ptr], L[b] = L[b], L[ptr]
+
+        # Are we finished? If not, iterate
+        L1_size = ptr - a
+        if L1_size == k: break
+
+        if L1_size > k:
+            b, depth = ptr-1, depth+1
+        else:
+            a, k, depth = ptr+1, k - L1_size - 1, depth+1
+
+    return depth, comp, e
 
 def test():
     # Build list of distinct numbers
@@ -28,7 +44,8 @@ def test():
     # Run experiments
     for _ in xrange(10):
         random.shuffle(L)
-        print sum([find(L, 0, len(L)-1, 500, 0)[0] for _ in xrange(1000)]) / 1000.0
+        acc = 0
+        print sum([find(L, 500)[0] for _ in xrange(1000)]) / 1000.0
 
 
 test()
