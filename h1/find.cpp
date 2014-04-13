2cf3835999a245281c9f0f29e44655c3c07aa20f
Bufas <bufas7@gmail.com>
2014-04-13 15:19:14 +0200

    Remove a swap

--- h1/find.cpp
+++ h1/find.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <utility>    // std::pair, std::swap
+#include <utility>    // std::pair
 #include <ctime>      // std::time()
 #include <cstdlib>    // std::rand, std::srand
 #include <vector>     // std::vector
@@ -12,10 +12,12 @@ using namespace std;
 
 pair<int, int> find(vector<int>& l, const vector<int>::iterator a, const vector<int>::iterator b, int k, int d) {
     // Pick the pivot and move it to the end
-    auto r = (distance(a, b) == 1) ? 0 : rand() % (distance(a, b) - 1);
+    int size = distance(a, b);
+    auto r = (size == 1) ? 0 : rand() % (size - 1);
     iter_swap(a + r, b-1);
     int e = *(b-1);
 
+    // Partition the array
     auto ptr = a;
     for (auto i = a; i != b-1; ++i) {
         if (*i < e) {
@@ -24,9 +26,6 @@ pair<int, int> find(vector<int>& l, const vector<int>::iterator a, const vector<
         }
     }
 
-    // Swap e to its right place
-    iter_swap(ptr, b-1);
-
     // Are we finished? If not, recurse
     int l1_size = distance(a, ptr);
     if (l1_size == k - 1) {
@@ -34,12 +33,12 @@ pair<int, int> find(vector<int>& l, const vector<int>::iterator a, const vector<
     }
 
     if (l1_size > k - 1) return find(l, a, ptr, k, d+1);
-    else return find(l, ptr+1, b, k - l1_size - 1, d+1);
+    else return find(l, ptr, b-1, k - l1_size - 1, d+1);
 }
 
 void shuffle(vector<int> &v) {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    shuffle (v.begin(), v.end(), std::default_random_engine(seed));
+    shuffle(v.begin(), v.end(), std::default_random_engine(seed));
 }
 
 int main() {
